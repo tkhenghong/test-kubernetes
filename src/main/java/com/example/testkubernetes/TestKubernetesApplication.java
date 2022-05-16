05b688e3745971ef7ff81e3bbe76548a2e73fb08
Teoh Kheng Hong <tkhenghong@gmail.com>
2022-05-16 22:39:57 +0800

    First commit

---
+++ src/main/java/com/example/testkubernetes/TestKubernetesApplication.java
@@ -0,0 +1,13 @@
+package com.example.testkubernetes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestKubernetesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestKubernetesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/testkubernetes/TestKubernetesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testkubernetes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestKubernetesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
